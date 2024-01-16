458973b45000c2a7b60e27944f7c518bfcddd08a
uncommon-type <uncommon.type@rambler.ru>
2024-01-16 11:12:00 +0000

    feat: implement <SignupButton/> component
    - Create a <SignupButton/> that can display different states (loading,
    error, success or default), depending on status prop passed to it
    
    — Add states object with loading, error, success and default properties,
    each representing different state that button can be in
    
    — Give each states object property object with button properties in
    that state

---
+++ src/components/pages/signup-form/signup-button.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+
+import Button from 'components/shared/button/button';
+
+const SignupButton = ({ status = 'default' }) => {
+  const states = {
+    loading: {
+      size: '2xs',
+      theme: 'plain',
+      icon: 'spinner',
+      className: 'animate-spin-slow',
+      ariaLabel: 'Loading',
+      disabled: true,
+    },
+    error: {
+      size: '2xs',
+      theme: 'plain',
+      icon: 'error',
+      disabled: true,
+      ariaLabel: 'Something went wrong',
+    },
+    success: {
+      size: '2xs',
+      theme: 'plain',
+      icon: 'success',
+      disabled: true,
+      ariaLabel: 'Email successfully submitted',
+    },
+    default: {
+      size: 'md',
+      theme: 'purple',
+      className: 'font-roboto-semibold font-semibold',
+      label: 'Free trial',
+      ariaLabel: 'Submit email for free trial',
+      icon: null,
+    },
+  };
+
+  const buttonProps = states[status];
+
+  return (
+    <Button
+      size={buttonProps.size}
+      theme={buttonProps.theme}
+      icon={buttonProps.icon}
+      className={buttonProps.className}
+      label={buttonProps.label}
+      aria-label={buttonProps.ariaLabel}
+      disabled={buttonProps.disabled}
+    />
+  );
+};
+
+SignupButton.propTypes = {
+  status: PropTypes.string,
+};
+
+export default SignupButton;
